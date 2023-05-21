b3a9e5135cf49eba48ae2502756ff69a930b507e
陳建成（Chien-Cheng (Jeff) Chen） <43311603+jeffeuxMartin@users.noreply.github.com>
2023-05-21 18:45:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -173,169 +173,172 @@ st.audio(get_audio(target_word))
 #         f"""臺語 proficiency: {split_name} 的有 {splits[split_name]}"""
 #     )
 
-for place_group in [north, centr, south, other]:
-    
-    oDATA = DATA[DATA["主觀出身"].isin(place_group)]
-    SPLIT_DFs = data_split(DATA, splits=splits)
+tabnorth, tabcentr, tabsouth, tabother = st.tabs(
+    ["北部", "中部", "南部", "其他"]
+)
+for place_group, place_tab in zip([north, centr, south, other], [tabnorth, tabcentr, tabsouth, tabother]):
+    with place_tab:
+        oDATA = DATA[DATA["主觀出身"].isin(place_group)]
+        SPLIT_DFs = data_split(DATA, splits=splits)
 
-    # %%
-    # st.dataframe(SPLIT_DFs["生疏"].head(10))
+        # %%
+        # st.dataframe(SPLIT_DFs["生疏"].head(10))
 
-    word = "<啥潲>"
-    word = target_word
-    生v = SPLIT_DFs["生疏"][word].value_counts()
-    普v = SPLIT_DFs["普通"][word].value_counts()
-    熟v = SPLIT_DFs["精熟"][word].value_counts()
-    portion = ([生v.sum(), 普v.sum(), 熟v.sum()])
-    portion = np.array(portion) / sum(portion)
-    portion /= portion.max()
-    生v_portion, 普v_portion, 熟v_portion = portion
-    # %%
-    fig = make_subplots(
-        rows=1, cols=3,
-        specs=[[
-            {"type": "pie"}, 
-            {"type": "pie"}, 
-            {"type": "pie"}, 
-        ]],
-        subplot_titles=(
-            "生疏", 
-            "普通", 
-            "精熟"
-        ),
-    )
+        word = "<啥潲>"
+        word = target_word
+        生v = SPLIT_DFs["生疏"][word].value_counts()
+        普v = SPLIT_DFs["普通"][word].value_counts()
+        熟v = SPLIT_DFs["精熟"][word].value_counts()
+        portion = ([生v.sum(), 普v.sum(), 熟v.sum()])
+        portion = np.array(portion) / sum(portion)
+        portion /= portion.max()
+        生v_portion, 普v_portion, 熟v_portion = portion
+        # %%
+        fig = make_subplots(
+            rows=1, cols=3,
+            specs=[[
+                {"type": "pie"}, 
+                {"type": "pie"}, 
+                {"type": "pie"}, 
+            ]],
+            subplot_titles=(
+                "生疏", 
+                "普通", 
+                "精熟"
+            ),
+        )
 
-    cols = st.columns(3)
-    bardata = {}
-    for k, col in zip(SPLIT_DFs, cols):
-        df = SPLIT_DFs[k]
-        portion = f"{df.shape[0] / DATA.shape[0] * 100:.2f} %"
-        length = f"{df.shape[0]}"
-        # col.metric(label=f"{k}\t({length} / {DATA.shape[0]})",
-        #            value=f"{portion}",
-        #            )
-        bardata[k] = {"counts": df.shape[0]}
-    split_data = {}
-    for number in range(1, 7 + 1):
-        split_data[number] = {"counts": DATA[DATA["臺熟"] == number].shape[0]}
-    # st.write(split_data)
-    # split_data = pd.DataFrame(split_data).T
-    # st.dataframe(split_data)
-    split_data = pd.DataFrame(split_data)
-    fig_p = px.bar(
-        split_data,
-        orientation="h",
-        height=170,
-        title=None,
-        labels={
-            "value": "Proficiency",
-        },
-        color_discrete_sequence=[
-            "#0000ff",
-            "#00ff33",
-            "#33cc00",
-            "#669900",
-            "#996600",
-            "#cc3300",
-            "#ff0000",
-        ],
-    )
-    fig_p.update_layout(
-        showlegend=False,
-        yaxis=dict(
+        cols = st.columns(3)
+        bardata = {}
+        for k, col in zip(SPLIT_DFs, cols):
+            df = SPLIT_DFs[k]
+            portion = f"{df.shape[0] / DATA.shape[0] * 100:.2f} %"
+            length = f"{df.shape[0]}"
+            # col.metric(label=f"{k}\t({length} / {DATA.shape[0]})",
+            #            value=f"{portion}",
+            #            )
+            bardata[k] = {"counts": df.shape[0]}
+        split_data = {}
+        for number in range(1, 7 + 1):
+            split_data[number] = {"counts": DATA[DATA["臺熟"] == number].shape[0]}
+        # st.write(split_data)
+        # split_data = pd.DataFrame(split_data).T
+        # st.dataframe(split_data)
+        split_data = pd.DataFrame(split_data)
+        fig_p = px.bar(
+            split_data,
+            orientation="h",
+            height=170,
             title=None,
-            tickmode="array",
-            tickvals=[],
-            ticktext=[],
-            showticklabels=False,
-            hoverformat="",
+            labels={
+                "value": "Proficiency",
+            },
+            color_discrete_sequence=[
+                "#0000ff",
+                "#00ff33",
+                "#33cc00",
+                "#669900",
+                "#996600",
+                "#cc3300",
+                "#ff0000",
+            ],
         )
-    )
-    st.plotly_chart(
-        fig_p,
-        use_container_width=True,
-    )
-        
-    bardata = pd.DataFrame(bardata)
-    # st.dataframe(bardata)
-    fig_bar = px.bar(
-        bardata,
-        orientation="h",
-        height=170,
-        # width=200,
-        title=None,
-        labels={
-        #     "variable": "Proficiency",
-            "value": "生疏 v.s 普通 v.s 精熟",
-            # "index": "",
-            "counts": "",
-        },
-        # hide row name
-        # hover_data=["counts"],
-        # hover_name="counts",
-        # text_auto="3d",
-        # text="counts",
-    )
-    fig_bar.update_layout(
-        showlegend=False,
-        # height=800,
-        # width=1600,
-        yaxis=dict(
+        fig_p.update_layout(
+            showlegend=False,
+            yaxis=dict(
+                title=None,
+                tickmode="array",
+                tickvals=[],
+                ticktext=[],
+                showticklabels=False,
+                hoverformat="",
+            )
+        )
+        st.plotly_chart(
+            fig_p,
+            use_container_width=True,
+        )
+            
+        bardata = pd.DataFrame(bardata)
+        # st.dataframe(bardata)
+        fig_bar = px.bar(
+            bardata,
+            orientation="h",
+            height=170,
+            # width=200,
             title=None,
-            tickmode="array",
-            tickvals=[],
-            ticktext=[],
-            showticklabels=False,
-            hoverformat="",
+            labels={
+            #     "variable": "Proficiency",
+                "value": "生疏 v.s 普通 v.s 精熟",
+                # "index": "",
+                "counts": "",
+            },
+            # hide row name
+            # hover_data=["counts"],
+            # hover_name="counts",
+            # text_auto="3d",
+            # text="counts",
         )
-    )
-    st.plotly_chart(
-        fig_bar,
-        use_container_width=True,
-    )
+        fig_bar.update_layout(
+            showlegend=False,
+            # height=800,
+            # width=1600,
+            yaxis=dict(
+                title=None,
+                tickmode="array",
+                tickvals=[],
+                ticktext=[],
+                showticklabels=False,
+                hoverformat="",
+            )
+        )
+        st.plotly_chart(
+            fig_bar,
+            use_container_width=True,
+        )
 
-    def pie_generator(df_valcounts, **kwargs):
-        return go.Pie(
-            labels=df_valcounts.index,
-            values=df_valcounts.values,
-            # scalegroup="one",
-            # name="",
-            **kwargs
+        def pie_generator(df_valcounts, **kwargs):
+            return go.Pie(
+                labels=df_valcounts.index,
+                values=df_valcounts.values,
+                # scalegroup="one",
+                # name="",
+                **kwargs
+            )
+        fig.add_trace(pie_generator(生v, hole=1 - 生v_portion), col=1, row=1)
+        fig.add_trace(pie_generator(普v, hole=1 - 普v_portion), col=2, row=1)
+        fig.add_trace(pie_generator(熟v, hole=1 - 熟v_portion), col=3, row=1)
+        # fig.update_annotations(font_size=30)
+        fig.update_traces(
+            textposition='inside', 
+            # textinfo='percent+label', 
+            textinfo='percent', 
+            # texttemplate="%{label}: %{value} <br />(%{percent})",
+            # texttemplate="%{label} (%{percent})",
+            textfont_size=15.5,
+            # marker=dict(
+            #     colors=["#ff0000", "#0000ff", "#00ff00"],
+            #     line=dict(color='#000000', width=2),
+            # ),
+            # hole=.7,
+            # hoverinfo="percent+name",
+            # hoverinfo="value",
+            hoverinfo="label+value",
+            # hovertemplate="%{label}: %{value} <br />(%{percent})",
+            # hovertemplate="%{value}",
+            # hoverlabel=dict(
+            #     bgcolor="white",
+            #     font_size=16,
+            #     font_family="Rockwell"
+            # ),
+            # disable hover trace
+                
+                
         )
-    fig.add_trace(pie_generator(生v, hole=1 - 生v_portion), col=1, row=1)
-    fig.add_trace(pie_generator(普v, hole=1 - 普v_portion), col=2, row=1)
-    fig.add_trace(pie_generator(熟v, hole=1 - 熟v_portion), col=3, row=1)
-    # fig.update_annotations(font_size=30)
-    fig.update_traces(
-        textposition='inside', 
-        # textinfo='percent+label', 
-        textinfo='percent', 
-        # texttemplate="%{label}: %{value} <br />(%{percent})",
-        # texttemplate="%{label} (%{percent})",
-        textfont_size=15.5,
-        # marker=dict(
-        #     colors=["#ff0000", "#0000ff", "#00ff00"],
-        #     line=dict(color='#000000', width=2),
-        # ),
-        # hole=.7,
-        # hoverinfo="percent+name",
-        # hoverinfo="value",
-        hoverinfo="label+value",
-        # hovertemplate="%{label}: %{value} <br />(%{percent})",
-        # hovertemplate="%{value}",
-        # hoverlabel=dict(
-        #     bgcolor="white",
-        #     font_size=16,
-        #     font_family="Rockwell"
-        # ),
-        # disable hover trace
-            
-            
-    )
-    fig.update_layout(
-        showlegend=True,
-        # height=800,
-        # width=1600,
-        title_text="Taigi Slang Analysis",
-    )
-    st.plotly_chart(fig, use_container_width=True)
+        fig.update_layout(
+            showlegend=True,
+            # height=800,
+            # width=1600,
+            title_text="Taigi Slang Analysis",
+        )
+        st.plotly_chart(fig, use_container_width=True)

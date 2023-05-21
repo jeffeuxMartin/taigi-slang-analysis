7256f9fe1ec9fa78c1359b89d44e4d732055fe7c
陳建成（Chien-Cheng (Jeff) Chen） <43311603+jeffeuxMartin@users.noreply.github.com>
2023-05-21 18:30:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -169,7 +169,7 @@ split_data = pd.DataFrame(split_data)
 fig_p = px.bar(
     split_data,
     orientation="h",
-    height=120,
+    height=180,
     title=None,
     labels={
         "value": "Proficiency",
@@ -205,7 +205,7 @@ bardata = pd.DataFrame(bardata)
 fig_bar = px.bar(
     bardata,
     orientation="h",
-    height=120,
+    height=180,
     # width=200,
     title=None,
     labels={
